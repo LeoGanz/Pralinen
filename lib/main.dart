7c5f8d35d8a561b905392cab034cd07ee54dfc57
Leonard Ganz <leonard@ruediger-ganz.de>
2021-09-01 12:07:56 +0200

    Hero animations for images

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:pralinen/praline.dart';
 import 'package:pralinen/theme_model.dart';
 import 'package:provider/provider.dart';
 
@@ -170,89 +171,6 @@ class _MyHomePageState extends State<MyHomePage> {
                     'Bei den Anti-Rentner-Pralinen Orange und Double Nuss handelt es sich um Placebopräparate'),
           ],
         ),
-      ),
-    );
-  }
-}
-
-class Praline extends StatelessWidget {
-  const Praline(
-      {Key? key,
-      required this.imagePath,
-      required this.name,
-      this.info = "",
-      this.nsfd = false,
-      this.isPlacebo = false})
-      : super(key: key);
-
-  final String imagePath;
-  final String name;
-  final String info;
-  final bool nsfd;
-  final bool isPlacebo;
-
-  @override
-  Widget build(BuildContext context) {
-    Color? orangeCardColor = context.watch<ThemeModel>().mode == ThemeMode.dark
-        ? Colors.deepOrange[700]
-        : Theme.of(context).cardColor; // or Colors.orange[100]
-    return Card(
-      elevation: 3,
-      color: name != 'Orange' ? Theme.of(context).cardColor : orangeCardColor,
-      child: Row(
-        crossAxisAlignment: CrossAxisAlignment.center,
-        children: [
-          Image.asset(
-            imagePath,
-            height: 120,
-            width: 120,
-          ),
-          Expanded(
-            child: Padding(
-              padding: const EdgeInsets.all(8.0),
-              child: Column(
-                mainAxisSize: MainAxisSize.max,
-                crossAxisAlignment: CrossAxisAlignment.stretch,
-                children: [
-                  Row(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      Text(
-                        "ARP: ",
-                        style: Theme.of(context).textTheme.headline5,
-                      ),
-                      Text(
-                        '$name',
-                        style: Theme.of(context).textTheme.headline6,
-                      ),
-                      Text(
-                        isPlacebo ? ' **' : "",
-                        style: Theme.of(context).textTheme.caption,
-                      ),
-                    ],
-                  ),
-                  SizedBox(height: 10),
-                  Text(
-                    '$info',
-                    style: Theme.of(context).textTheme.bodyText2,
-                  ),
-                  SizedBox(height: 10),
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.end,
-                    children: [
-                      Text(
-                        nsfd ? "NSFD" : "",
-                        textAlign: TextAlign.end,
-                      ),
-                      Text(nsfd ? '*' : "",
-                          style: Theme.of(context).textTheme.caption),
-                    ],
-                  )
-                ],
-              ),
-            ),
-          ),
-        ],
       ),
     );
   }
